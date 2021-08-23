b27679d94e30bd1aad58485a345fe2bc829f293a
Lubos Verzich <lve@paymorrow.com>
2021-08-23 21:15:14 +0200

    Added exchange panel

--- src/view/trading/TradingPage.tsx
+++ src/view/trading/TradingPage.tsx
@@ -5,6 +5,9 @@ import {globalContext} from "context/GlobalContext"
 import {PlayerStore} from "context/PlayerStore"
 import ResourceContext from "context/ResourceContext"
 
+import balanceIcon from "assets/icons/balance.svg"
+import {MONEY} from "constant/Constants"
+
 export default function TradingPage() {
 	const context = useContext(globalContext)
 	const [selectedResource, setSelectedResource] = useState<ResourceContext | undefined>(undefined)
@@ -29,6 +32,7 @@ export default function TradingPage() {
 	return useObserver(() =>
 		<div>
 			<ResourcesGrid playerStore={context.playerStore} onSelectResourceCallback={setSelectedResource} selectedResource={selectedResource}/>
+			<ExchangePanel selectedResource={selectedResource}/>
 			<div className={"flex justify-around"}>
 				<Menu callback={buyResources} text={"Buy"} buttonColor={"bg-green-700"}/>
 				<Graph/>
@@ -37,6 +41,24 @@ export default function TradingPage() {
 		</div>
 	)
 }
+
+const ExchangePanel = observer((props: { selectedResource?: ResourceContext }) => {
+	return (
+		<div className={"flex flex-row md:p-8 md:m-16 sm:p-4 sm:m-4 justify-center bg-gray-300 items-center rounded-2xl"}>
+			<div className={"md:w-40 md:h-40 sm:w-20 sm:h-20 bg-gray-200 mx-8 border-gray-500 border-4 shadow-inner rounded-2xl"}>
+				<img src={props.selectedResource?.resource.icon} alt={""}/>
+			</div>
+			<div className={"flex flex-row items-center"}>
+				<div className={"md:w-40 md:h-40 sm:w-20 sm:h-20 flex flex-col justify-center items-center "}>
+					<img src={balanceIcon} color={"black"} className={"opacity-10"} alt={""}/>
+				</div>
+			</div>
+			<div className={"md:w-40 md:h-40 sm:w-20 sm:h-20 bg-gray-200 mx-8 border-gray-500 border-4 shadow-inner rounded-2xl"}>
+				<img src={MONEY.gold.icon} alt={""}/>
+			</div>
+		</div>
+	)
+})
 
 
 const Menu = observer((props: { callback: Function, text: string, buttonColor: string }) => {
@@ -76,7 +98,7 @@ const ResourcesGrid = observer((props: ResourceGridProps) => {
 	return (
 		<div className={"grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 m-4 gap-10 mb-20"}>
 			{
-				props.playerStore.ownedResources.map(res =>
+				props.playerStore.ownedResources.filter(res => !Object.values(MONEY).includes(res.resource)).map(res =>
 					<div key={res.resource.name} className={`${res === props.selectedResource ? "border-purple-500 border-2 rounded" : ""}`}>
 						<div className={`select-none shadow-md p-2 bg-gray-100`}
 						     onClick={() => props.onSelectResourceCallback(res)}>
